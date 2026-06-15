b9cf84a1dd93a1473e0c5b90d2fdbfc646bceddb
wnsnk <236621219+wnsnk@users.noreply.github.com>
2026-06-15 23:16:31 +0200

    add checking how many houses user applied to

--- main.py
+++ main.py
@@ -5,13 +5,7 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.firefox.options import Options
 from selenium.webdriver.firefox.firefox_profile import FirefoxProfile
 from selenium.webdriver.support import expected_conditions as EC
-# Source - https://stackoverflow.com/a/32887994
-# Posted by Saurabh Shrivastava, modified by community. See post 'Timeline' for change history
-# Retrieved 2026-06-15, License - CC BY-SA 4.0
-
 from selenium.webdriver.common.action_chains import ActionChains
-
-
 import time
 from dotenv import load_dotenv
 import os
@@ -19,6 +13,7 @@ import os
 load_dotenv()
 
 URL = 'https://www.thuisindeachterhoek.nl/'
+MAX_RESPONSES = 3
 username = os.getenv('THUIS_IN_DE_ACHTERHOEK_USERNAME')
 password = os.getenv('THUIS_IN_DE_ACHTERHOEK_PASSWORD')
 driver = webdriver.Firefox()
@@ -31,12 +26,14 @@ WebDriverWait(driver, 20).until(EC.element_to_be_clickable(
     (By.CSS_SELECTOR, '#CybotCookiebotDialogBodyButtonDecline'))).click()
 WebDriverWait(driver, 20).until(EC.element_to_be_clickable(
     (By.CSS_SELECTOR, '.login-plugin-container'))).click()
+time.sleep(1)
 
 
 def log_in():
     username_entry = WebDriverWait(driver, 20).until(
         EC.element_to_be_clickable((By.NAME, 'username')))
     username_entry.click()
+
     actions.send_keys(username)
     actions.perform()
     password_entry = WebDriverWait(driver, 20).until(
@@ -47,5 +44,21 @@ def log_in():
     actions.perform()
 
 
+def check_my_applications():
+    '''Returns the num of current applications for houses.'''
+    my_applications_button = WebDriverWait(driver, 20).until(
+        EC.element_to_be_clickable((By.CSS_SELECTOR, 'ul.dashboard-menu:nth-child(3) > li:nth-child(2)')))
+    my_applications_button.click()
+    time.sleep(2)
+    WebDriverWait(driver, 20).until(
+        EC.element_to_be_clickable((By.CLASS_NAME, 'reactie')))
+    responses = driver.find_elements(By.CLASS_NAME, 'reactie')
+    responses.pop()  # removes "see relevant offers"
+    return len(responses)
+
+
 log_in()
+a = check_my_applications()
+print(a)
+
 time.sleep(10000)
